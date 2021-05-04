8c1695983538f3c7946a4b44fab39fb612da1421
Fadi <haddad0207@gmail.com>
2021-05-04 15:28:37 +0300

    .

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Match 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$249.67</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
